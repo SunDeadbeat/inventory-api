585ca85c7604341182b4296566acc2b52fa28e20
Tonatiu Cruz <tonatiu_98x@hotmail.com>
2025-02-13 18:39:21 -0600

    Add inventory schema and model

---
+++ src/modules/inventory/inventory.schema.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from 'mongoose';
+
+export interface Inventory {
+    id: string;
+    productId: string;
+    storeId: string;
+    quantity: number;
+    minStock: number;
+}
+
+const schema = new Schema<Inventory>({
+    id: { type: String, required: true, unique: true },
+    productId: { type: String, required: true },
+    storeId: { type: String, required: true },
+    quantity: { type: Number, required: true, min: 0 },
+    minStock: { type: Number, required: true, min: 0 },
+}, {
+    collection: 'inventory',
+    timestamps: true,
+    versionKey: false
+});
+
+export default model<Inventory>('inventory', schema);
