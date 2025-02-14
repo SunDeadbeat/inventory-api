701b7c610f772a6e0d594943d9c42291bde4cd13
Tonatiu Cruz <tonatiu_98x@hotmail.com>
2025-02-13 18:41:11 -0600

    Add movement schema and model

---
+++ src/modules/movement/movement.schema.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose';
+import { MovementType } from '../../enums/movement.enum';
+
+export interface Movement {
+    id: string;
+    productId: string;
+    sourceStoreId: string;
+    targetStoreId: string;
+    quantity: number;
+    timestamp: Date;
+    type: MovementType;
+}
+
+const schema = new Schema<Movement>({
+    id: { type: String, required: true, unique: true },
+    productId: { type: String, required: true },
+    sourceStoreId: { type: String, required: true },
+    targetStoreId: { type: String, required: true },
+    quantity: { type: Number, required: true, min: 0 },
+    timestamp: { type: Date, required: true },
+    type: { type: String, required: true, enum: Object.values(MovementType) },
+}, {
+    collection: 'movement',
+    timestamps: true,
+    versionKey: false
+});
+
+export default model<Movement>('movement', schema);
