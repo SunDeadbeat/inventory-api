c09bc8f412dba7ccf3d0065af131a907bd3f03f7
Tonatiu Cruz <tonatiu_98x@hotmail.com>
2025-02-14 13:32:34 -0600

    Add product routes for CRUD operations

---
+++ src/modules/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { ProductController } from './product/product.controller.class';
+
+const router: Router = Router();
+
+router.get('/products', ProductController.find);
+router.get('/products/:id', ProductController.findOne);
+router.post('/products', ProductController.create);
+router.put('/products/:id', ProductController.update);
+router.delete('/products/:id', ProductController.delete);
+
+export = router;
