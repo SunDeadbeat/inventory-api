0b13488214a701b503b2b3c022ee6ac1e10e7bf8
Tonatiu Cruz <tonatiu_98x@hotmail.com>
2025-02-15 01:07:31 -0600

    Add route to get inventory by store ID in index module

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { ProductController } from './product/product.controller.class';
+import { InventoryController } from './inventory/inventory.controller.class';
 
 const router: Router = Router();
 
@@ -8,5 +9,6 @@ router.get('/products/:id', ProductController.findOne);
 router.post('/products', ProductController.create);
 router.put('/products/:id', ProductController.update);
 router.delete('/products/:id', ProductController.delete);
+router.get('/stores/:storeId/inventory', InventoryController.findByStoreId);
 
 export = router;
